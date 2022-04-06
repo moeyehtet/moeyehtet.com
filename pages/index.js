761e24361e5a70b77ca403004c12e9ef637b8e40
Moe Ye Htet <moeyehtet96@gmail.com>
2022-04-06 18:13:50 +0630

    Added metadata and favicon for SEO

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,27 @@ import Layout from "../components/Layout";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Resume from "../components/Resume";
-import { Divider, useMediaQuery } from "@chakra-ui/react";
+import { Divider } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function Home() {
     return (
         <React.Fragment>
+            <Head>
+                <title>Moe Ye Htet</title>
+                <meta
+                    name="description"
+                    content="Moe Ye Htet's Personal Website"
+                />
+                <meta property="og:title" content="Moe Ye Htet" />
+                <meta
+                    property="og:description"
+                    content="Moe Ye Htet's Personal Website"
+                />
+                <meta property="og:url" content="https://moeyehtet.com" />
+                <meta property="og:type" content="website" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <Layout>
                 <About my="4" />
                 <Divider maxW="md" mx="auto" />
