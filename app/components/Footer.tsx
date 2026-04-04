8c8b8685e35ead6475986c27ee0047babd06b0cd
Moe Ye Htet <moeyehtet96@gmail.com>
2026-04-04 11:43:39 +0200

    Remove used technologies in Footer component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <footer className="text-center py-4">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <p className="text-sm text-nord4">
-          Copyright © {new Date().getFullYear()} Moe Ye Htet. All rights
-          reserved.
-        </p>
-        <p className="text-xs text-nord4">
-          Created with Next.js, Tailwind CSS and Preline.
-        </p>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="text-center py-4">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <p className="text-sm text-nord4">
+                    Copyright © {new Date().getFullYear()} Moe Ye Htet. All
+                    rights reserved.
+                </p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
